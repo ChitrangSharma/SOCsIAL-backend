2faa9a49cd01c25d65b30247da9e436d1d45e5a3
ChitrangSharma <55293779+ChitrangSharma@users.noreply.github.com>
2021-04-07 00:10:36 +0530

    abstraction

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports={
-    mongoURI:"mongodb+srv://chitrang:bXwoen2KMNhR6h1a@cluster0.ibthb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI:"...."
     ,
-    JWT_SECRET:"fuckitbowlfast"
+    JWT_SECRET:"..."
 }
